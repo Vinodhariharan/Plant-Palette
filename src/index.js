d2c302a4ff76c1c4a201c45b748b2e81e8810645
Vinodhariharan <vinodhari.ravi@outlook.com>
2024-04-09 10:51:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import { HashRouter } from 'react-router-dom/cjs/react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
